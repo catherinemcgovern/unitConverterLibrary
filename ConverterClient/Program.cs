7b77a2eee6a7ccd845a70e306b8e3c241a60a414
cmcgovern4ever <cmcgovern@wtamu.edu>
2018-10-31 11:42:39 -0500

    first commit

---
+++ ConverterClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ConverterLib;
+
+namespace ConverterClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyTemperatureConverter converter = new MyTemperatureConverter();
+
+            Console.WriteLine(converter.CelsiusToFarenheit(15));
+        }
+    }
+}
---
+++ ConverterLib/TemperatureConversions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConverterLib
+{
+    interface ITemperatureConverter
+    {
+        decimal CelsiusToFarenheit(decimal TempInCelsius);
+
+
+    }
+    public class MyTemperatureConverter : ITemperatureConverter
+    {
+        public decimal CelsiusToFarenheit(decimal TempInCelsius)
+        {
+            //F=C*(9/5) +32
+            return TempInCelsius * (9m/5m) + 32;
+        }
+
+    }
+
+
+
+    
+    public class AlternativeTemperatureConverter : ITemperatureConverter
+    {
+        public decimal CelsiusToFarenheit(decimal TempInCelsius)
+        {
+            //F=C*(9/5) +32
+            return TempInCelsius * (1.8m) + 32;
+        }
+        
+    }
+
+}
+
